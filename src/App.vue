4212266a9355a781665c514623379f703b4656d2
jackunWeb <jackun.jack@gmail.com>
2021-12-23 16:23:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    {{info}}
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: 'App',
+  data:()=>({
+    info:null
+  }),
+  mounted:function(){
+    axios
+      .get(document.URL+'api/foo')
+      .then(response => (this.info = response.data.msg));
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
